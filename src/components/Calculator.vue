0e46accf87b7072a63ead9dfe1927f6fe8f2caac
Artem Afonin <joniclife@gmail.com>
2020-01-03 17:38:37 +0700

    Remove x-axis padding

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -18,38 +18,38 @@
         </div>
 
         <div class="row mx-5 mb-1">
-            <div class="col bg-dark text-center text-white-50 mx-1 btn">C</div>
-            <div class="col bg-dark text-center text-white-50 mx-1 btn">+/-</div>
-            <div class="col bg-dark text-center text-white-50 mx-1 btn">%</div>
-            <div class="col bg-dark text-center text-white-50 mx-1 btn">/</div>
+            <div class="col bg-dark text-center text-white-50 px-0 mx-1 btn">C</div>
+            <div class="col bg-dark text-center text-white-50 px-0 mx-1 btn">+/-</div>
+            <div class="col bg-dark text-center text-white-50 px-0 mx-1 btn">%</div>
+            <div class="col bg-dark text-center text-white-50 px-0 mx-1 btn">/</div>
         </div>
 
         <div class="row mx-5 mb-1">
-            <div class="col bg-light text-center text-black-50 mx-1 btn border-dark">7</div>
-            <div class="col bg-light text-center text-black-50 mx-1 btn border-dark">8</div>
-            <div class="col bg-light text-center text-black-50 mx-1 btn border-dark">9</div>
-            <div class="col bg-dark text-center text-white-50 mx-1 btn">*</div>
+            <div class="col bg-light text-center text-black-50 px-0 mx-1 btn border-dark">7</div>
+            <div class="col bg-light text-center text-black-50 px-0 mx-1 btn border-dark">8</div>
+            <div class="col bg-light text-center text-black-50 px-0 mx-1 btn border-dark">9</div>
+            <div class="col bg-dark text-center text-white-50 px-0 mx-1 btn">*</div>
         </div>
 
         <div class="row mx-5 mb-1">
-            <div class="col bg-light text-center text-black-50 mx-1 btn border-dark">4</div>
-            <div class="col bg-light text-center text-black-50 mx-1 btn border-dark">5</div>
-            <div class="col bg-light text-center text-black-50 mx-1 btn border-dark">6</div>
-            <div class="col bg-dark text-center text-white-50 mx-1 btn">-</div>
+            <div class="col bg-light text-center text-black-50 px-0 mx-1 btn border-dark">4</div>
+            <div class="col bg-light text-center text-black-50 px-0 mx-1 btn border-dark">5</div>
+            <div class="col bg-light text-center text-black-50 px-0 mx-1 btn border-dark">6</div>
+            <div class="col bg-dark text-center text-white-50 px-0 mx-1 btn">-</div>
         </div>
 
         <div class="row mx-5 mb-1">
-            <div class="col bg-light text-center text-black-50 mx-1 btn border-dark">1</div>
-            <div class="col bg-light text-center text-black-50 mx-1 btn border-dark">2</div>
-            <div class="col bg-light text-center text-black-50 mx-1 btn border-dark">3</div>
-            <div class="col bg-dark text-center text-white-50 mx-1 btn">+</div>
+            <div class="col bg-light text-center text-black-50 px-0 mx-1 btn border-dark">1</div>
+            <div class="col bg-light text-center text-black-50 px-0 mx-1 btn border-dark">2</div>
+            <div class="col bg-light text-center text-black-50 px-0 mx-1 btn border-dark">3</div>
+            <div class="col bg-dark text-center text-white-50 px-0 mx-1 btn">+</div>
         </div>
 
         <div class="row mx-5 mb-1">
-            <div class="col mx-1"></div>
-            <div class="col bg-light text-center text-black-50 mx-1 btn border-dark">0</div>
-            <div class="col bg-light text-center text-black-50 mx-1 btn border-dark">.</div>
-            <div class="col bg-dark text-center text-white-50 mx-1 btn con">=</div>
+            <div class="col px-0 mx-1">&nbsp;</div>
+            <div class="col bg-light text-center text-black-50 px-0 mx-1 btn border-dark">0</div>
+            <div class="col bg-light text-center text-black-50 px-0 mx-1 btn border-dark">.</div>
+            <div class="col bg-dark text-center text-white-50 px-0 mx-1 btn con">=</div>
         </div>
     </div>
 </template>

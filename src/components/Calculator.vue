ed87c3bd049f4357ed05b123891e7f300627c683
Artem Afonin <joniclife@gmail.com>
2020-01-03 16:40:13 +0700

    Create calculator template

---
+++ src/components/Calculator.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="row">
+        <div class="col bg-primary rounded">
+            <h3 class="h3 text-center text-black-50">
+                <slot/>
+            </h3>
+        </div>
+    </header>
+</template>
+
+<script>
+  export default {
+    name: "Calculator"
+  }
+</script>
+
+<style scoped>
+
+</style>
